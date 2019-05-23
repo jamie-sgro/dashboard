be930ebc396025c7b206665109e153b36d0eee6f
Jamie <iamjamies@gmail.com>
2019-05-23 09:47:34 -0400

    set up stable workaround for main.js - cannot access /ui/

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,13 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('ui', 'html');
+  res.render('ui');
   //res.render('index', { title: 'Express' });
 });
 
 /* GET User Interface page. */
 router.get('/ui/', (req, res) => {
-  res.render('ui', 'html');
+  res.render('ui');
   //res.render('index', { title: 'Express' });
   return;
 });
