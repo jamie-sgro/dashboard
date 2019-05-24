8463df5cbf8091be9fd4e57b24825d791ea5974a
Jamie <iamjamies@gmail.com>
2019-05-24 10:28:12 -0400

    softcoded addMarker function

--- server/public/ui/main.js
+++ server/public/ui/main.js
@@ -8,18 +8,11 @@ const locHost = "http://localhost:3000/";
 
 
 // import .csv
-
-
-
-
 function postAjax(url, data, callback) {
   $.ajax({
     type: "POST",
     data: JSON.stringify(data),
-    //data: data,
     url: url,
-    //contentType: 'application/json',
-    //datatype: "json"
   }).done((data)=> {
     callback(null, data);
   }).fail((jqXHR, textStatus, errorThrown)=> {
@@ -27,10 +20,26 @@ function postAjax(url, data, callback) {
   });
 };
 
-postAjax(locHost + "getData", {}, (err, cb)=> {
-  // TODO: add error handling
-  data = cb
-});
+data = getData();
+
+async function getData() {
+  return new Promise((resolve, reject) => {
+    postAjax(locHost + "getData", {}, (err, cb)=> {
+      if (err) {
+        try {
+          errMsg = JSON.parse(err.responseText)
+          console.log(errMsg.name + ": " + errMsg.message);
+        } catch(e) {
+          console.log(err.statusText);
+        };
+        return;
+      };
+
+      resolve(cb);
+      return;
+    });
+  });
+};
 
 
 
@@ -51,17 +60,26 @@ L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     }).addTo(map);
 
 
-// add marker
 
-var marker = L.marker([36.9375, -121.3542]).addTo(map);
-marker.on("click", ()=> {
-  console.log("Marker clicked!")
-})
+
+addMarker("San Jose-Sunnyvale-Santa Clara, CA ", "36.9375", "-121.3542", "68.57");
+
+function addMarker(name, lat, lng, score) {
+  var mark = L.marker([lat, lng]).addTo(map);
+  mark.on("click", ()=> {
+    //this is where hooks into .d3 should be made
+    console.log("Marker clicked!");
+  })
+  mark.on("mouseover", ()=> {
+
+  });
+  mark.bindPopup(score);
+  mark.name = name;
+  return(mark);
+}
 
 //If I want the popup open by default
 //marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-
-marker.bindPopup("68.57")
 
 //set up alerts
 map.on("click", onMapClick);
