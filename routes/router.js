5b946239880e3faae866c1debe934ecc80737a88
Jamie <iamjamies@gmail.com>
2019-06-05 15:19:41 -0400

    followed example html css template

--- routes/router.js
+++ routes/router.js
@@ -8,6 +8,11 @@ router.get('/', (req, res)=> {
   res.render('index');
 });
 
+//GET about page.
+router.get('/about', (req, res)=> {
+  res.render('about');
+});
+
 //GET User Interface page.
 router.get('/ui/', (req, res) => {
   console.log("get: '/ui/'")
@@ -16,7 +21,7 @@ router.get('/ui/', (req, res) => {
   return;
 });
 
-router.post('/getData', (req, res, next) => {
+router.post('/getData', (req, res) => {
   var fs = require("fs")
   var rawData = fs.readFileSync("./public/data/sdsn_cleaned.csv", "utf8");
 
