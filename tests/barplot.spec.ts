36a2903e95391b93fa712af3adbe36ef6212a653
jamie-sgro <jamie.sgro@uwaterloo.ca>
2021-06-26 12:19:44 -0400

    renamed test.ts to barplot.spec.ts

---
+++ tests/barplot.spec.ts
@@ -0,0 +1,18 @@
+// @ts-expect-error
+d3 = require("d3")
+
+const chaiSrc = require("chai")
+const barplotSrc = require("../src/barplot")
+
+describe('Base', function() {
+  it('should have basic chai and mocha validation', function() {
+    chaiSrc.expect("1").to.equal("1")
+  });
+});
+
+describe('Barplot', function() {
+  it('should return "a"', function() {
+    let result = barplotSrc.setAlpha("rgb(0,0,0,)", 1)
+    chaiSrc.expect(result.opacity).to.equal(1);
+  });
+});
