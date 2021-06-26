caf199b2316998b7b9dfaec53a33e8cf8854278f
jamie-sgro <jamie.sgro@uwaterloo.ca>
2021-06-26 12:13:36 -0400

    updated test.ts

--- test/test.ts
+++ test/test.ts
@@ -1,12 +1,27 @@
-require('jsdom-global')()
+// @ts-expect-error
+d3 = require("d3")
 
+const chaiSrc = require("chai")
+const clientSrc = require("../client")
+const barplotSrc = require("../barplot")
 
-import { expect } from 'chai';
-import { setAlpha } from '../src/barplot';
-
-describe("barplot", () => {
-  it("should update alpha", function() {
-    let result = setAlpha("rgb(0,0,0,)", 1)
-    expect(result.opacity).to.equal(1);
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      console.log("asweadef")
+      chaiSrc.expect("1").to.equal("1")
+    });
+  });
+  describe('Client', function() {
+    it('should return "a"', function() {
+      let client = new clientSrc.Client()
+      chaiSrc.expect(client.get_a()).to.equal("a")
+    });
+  });
+  describe('Barplot', function() {
+    it('should return "a"', function() {
+      let result = barplotSrc.setAlpha("rgb(0,0,0,)", 1)
+      chaiSrc.expect(result.opacity).to.equal(1);
+    });
   });
 });
