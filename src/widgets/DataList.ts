60f80c43621d9e9a20260d1b2b9f32d2d2c2931e
jamie-sgro <jamie.sgro@uwaterloo.ca>
2021-06-26 23:05:01 -0400

    refactored DataList()

--- src/widgets/DataList.ts
+++ src/widgets/DataList.ts
@@ -10,8 +10,24 @@ export class DataList {
     this.data = data;
     this.view = this.initView();
   }
+  
+    private uuidv4(): string {
+      // @ts-expect-error
+      return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+        (
+          c ^
+          (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+        ).toString(16)
+      );
+    }
 
   private initView(): HTMLInputElement {
+    let elem = this.createInput();
+    this.fillDataList();
+    return elem;
+  }
+
+  private createInput() {
     let elem = document.createElement("input");
     elem.id = this.htmlId;
     elem.setAttribute("list", this.listAttr);
@@ -20,18 +36,15 @@ export class DataList {
       console.log(e.target.value);
     };
     document.body.appendChild(elem);
-
-    this.fillDataList()
-
     return elem;
   }
 
   private fillDataList() {
-    let datalist = this.getDataList();
+    let datalist = this.createDataList();
     this.data.forEach((d) => this.addSingleOption(datalist, d));
   }
 
-  private getDataList(): HTMLDataListElement {
+  private createDataList(): HTMLDataListElement {
     let elem = document.getElementById(this.htmlId);
     let datalist = document.createElement("datalist");
     datalist.id = this.listAttr;
@@ -43,15 +56,5 @@ export class DataList {
     let option = document.createElement("option");
     option.value = aString;
     datalist.appendChild(option);
-  }
-
-  private uuidv4(): string {
-    // @ts-expect-error
-    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
-      (
-        c ^
-        (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
-      ).toString(16)
-    );
   }
 }
