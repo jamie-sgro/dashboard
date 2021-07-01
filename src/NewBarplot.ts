443cfed40e5a32900d3b5a85c0082f0815ab7f38
jamie-sgro <jamie.sgro@uwaterloo.ca>
2021-07-01 09:21:49 -0400

    added method to retrieve client rectangle of div elem

--- src/NewBarplot.ts
+++ src/NewBarplot.ts
@@ -1,23 +1,30 @@
 // @ts-expect-error
 import d3 = require("d3");
+import { uuidv4 } from "./utils";
 
 export class NewBarplot {
-  parentId: string;
-  svg: d3.Selection<SVGSVGElement, unknown, HTMLElement, any>;
+  private parentId: string;
+  private htmlId: string;
+  private svg: d3.Selection<SVGSVGElement, unknown, HTMLElement, any>;
 
   constructor(parentId) {
     this.parentId = parentId;
+    this.htmlId = uuidv4();
     this.svg = d3
       .select(this.parentId)
       .append("svg")
+      .attr("id", this.htmlId)
       .call(this.getSvgSize, this);
   }
 
   getSvgSize(path, obj) {
-    path
-      .attr("width", obj.width)
-      .attr("height", obj.height);
+    // @ts-expect-error
+    let clientRect: ClientRect = d3.select(`#${obj.parentId}`).node().getBoundingClientRect()
+    let width = clientRect.width
+    let height = clientRect.height
 
-    $(".barplot.svg").css({ left: $(window).width() });
+    path.attr("width", width).attr("height", height);
+
+    // $(`#${obj.htmlId}`).css({ left: $(window).width() });
   }
 }
