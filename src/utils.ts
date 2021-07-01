b45cc4f83bb6f073c04c50e6d038faee4ddcec17
jamie.sgro <iamjamieswork@gmail.com>
2021-07-01 09:21:49 -0400

    added method to retrieve client rectangle of div elem

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,13 @@
 export function assertType(toCheck, type: Function) {
   if (!(toCheck instanceof type)) {
-    throw new TypeError(typeof(toCheck) + " not of type: " + type.name) 
+    throw new TypeError(typeof toCheck + " not of type: " + type.name);
   }
+}
+
+export function uuidv4(): string {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
 }
