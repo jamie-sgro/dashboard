d201377d5b9e0979e45a1b46abab183f9b66c3b8
jamie-sgro <jamie.sgro@uwaterloo.ca>
2021-07-01 12:23:37 -0400

    added rectangle to match div elem

--- src/NewBarplot.ts
+++ src/NewBarplot.ts
@@ -1,14 +1,17 @@
 // @ts-expect-error
 import d3 = require("d3");
-import { uuidv4 } from "./utils";
+import { uuidv4, assert } from "./utils";
 
 export class NewBarplot {
   private parentId: string;
   private htmlId: string;
   private svg: d3.Selection<SVGSVGElement, unknown, HTMLElement, any>;
-  private canvas: d3.Selection<SVGGElement, unknown, HTMLElement, any>
+  private canvas: d3.Selection<SVGGElement, unknown, HTMLElement, any>;
+  width: number;
+  height: number;
 
-  constructor(parentId) {
+  constructor(parentId: string) {
+    assert(parentId.charAt(0) === "#");
     this.parentId = parentId;
     this.htmlId = uuidv4();
     this.svg = d3
@@ -19,18 +22,21 @@ export class NewBarplot {
 
     this.canvas = this.svg
       .append("rect")
-        .attr("x", 150)
-        .attr("y", 50)
-        .attr("width", 50)
-        .attr("height", 140);
+      .attr("x", 0)
+      .attr("y", 0)
+      .attr("width", this.width)
+      .attr("height", this.height);
   }
 
   getSvgSize(path, obj) {
-    let clientRect: ClientRect = d3.select(obj.parentId).node().getBoundingClientRect()
-    let width = clientRect.width
-    let height = clientRect.height
+    let clientRect: ClientRect = d3
+      .select(obj.parentId)
+      .node()
+      .getBoundingClientRect();
+    obj.width = clientRect.width;
+    obj.height = clientRect.height;
 
-    path.attr("width", width).attr("height", height);
+    path.attr("width", obj.width).attr("height", obj.height);
 
     // $(`#${obj.htmlId}`).css({ left: $(window).width() });
   }
