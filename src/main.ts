f57ea5bcd17cb57c69b0e93b41d9c9218f160bcf
jamie-sgro <iamjamieswork@gmail.com>
2021-10-30 18:49:14 -0400

    added new radio set that toggles the city rank chart

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,25 @@
 // @ts-expect-error
-import d3 = require('d3');
+import d3 = require("d3");
 
-import { Barplot } from './barplot.js';
-import { Data } from './data.js';
+import { Barplot } from "./barplot.js";
+import { Data, DataPoint } from "./data.js";
 import {
   populateMarkers,
   recenterDashboard,
   updateAllGraphs,
   updateGraphById,
-} from './map.js';
-import { Margin } from './Margin.js';
-import { panel3Resize } from './panel3.js';
-import { ToggleButton } from './widgets/ToggleButton.js';
-import { DataListModel } from './widgets/DataListModel.js';
-import { RadioButton } from './widgets/RadioButton.js';
-import { VerticalLine } from './annotations/VerticalLine.js';
+} from "./map.js";
+import { Margin } from "./Margin.js";
+import { panel3Resize } from "./panel3.js";
+import { ToggleButton } from "./widgets/ToggleButton.js";
+import { DataListModel } from "./widgets/DataListModel.js";
+import { RadioButton } from "./widgets/RadioButton.js";
+import { VerticalLine } from "./annotations/VerticalLine.js";
 
 export const markRad = 15;
-export const markCol = 'rgba(10,151,217, .8)';
-export const colourBottom = 'rgb(56, 94, 231)';
-export const colourTop = 'rgb(34, 236, 87)';
+export const markCol = "rgba(10,151,217, .8)";
+export const colourBottom = "rgb(56, 94, 231)";
+export const colourTop = "rgb(34, 236, 87)";
 export const scaleToZoom = false;
 export const panelHeight = 0.4;
 export const panelWidth = 0.33;
@@ -33,22 +33,17 @@ export const panelWidth = 0.33;
  **********************/
 
 let avgBarplot = new Barplot(
-  '#column-3',
+  "#column-3",
   $(window).width() * panelWidth,
   getHeight(),
   { margin: new Margin(10, 20, 30, 60) }
 );
-$(window).on('resize', function () {
+$(window).on("resize", function () {
   avgBarplot.resize();
 });
 
-// let rankingMethod = Data.getPseudoGeometricMeanForCity
-// let meanCountry =  Data.getAverageCountry(rankingMethod)
-
-// let rankingMethod = Data.getArithmenticMeanForCity
-// let meanCountry =  Data.getAverageCountry(rankingMethod)
-
-let meanCountry = Data.get_condorcet_ranking_all_variables()
+let rankingMethod = Data.getArithmenticMeanForCity
+let meanCountry =  Data.getAverageCountry(rankingMethod)
 avgBarplot.drawAverageCountry(meanCountry);
 
 /******************
@@ -71,7 +66,7 @@ export var mark = populateMarkers();
  *********************/
 
 //set up svg ahead so the modular function can select instead of append
-d3.select('#panel3').append('svg');
+d3.select("#panel3").append("svg");
 
 // panel3Resize();
 
@@ -82,25 +77,25 @@ d3.select('#panel3').append('svg');
  *********************/
 
 function getHeight(): number {
-  if ($('#header').height()) {
-    return $(window).height() - $('#header').height();
+  if ($("#header").height()) {
+    return $(window).height() - $("#header").height();
   } else {
     return $(window).height();
   }
 }
 
 export const barplot = new Barplot(
-  '#column-2',
+  "#column-2",
   $(window).width() * panelWidth,
   getHeight(),
   { margin: new Margin(10, 20, 30, 60) }
 );
 plotData(barplot);
 barplot.annotation.verticalLines.push(
-  new VerticalLine(barplot, 0, { text: 'Farthest from Target' })
+  new VerticalLine(barplot, 0, { text: "Farthest from Target" })
 );
 barplot.annotation.verticalLines.push(
-  new VerticalLine(barplot, 1, { text: 'Target', colour: colourBottom })
+  new VerticalLine(barplot, 1, { text: "Target", colour: colourBottom })
 );
 
 function plotData(barplot: Barplot) {
@@ -118,43 +113,82 @@ function plotData(barplot: Barplot) {
   updateGraphById(id, barplot);
 }
 
-let header = document.createElement('h1');
+let header = document.createElement("h1");
 header.textContent = Data.getSyncData()[0].name;
-document.getElementById('column-1').appendChild(header);
+document.getElementById("column-1").appendChild(header);
 
 function leadLagOnClick() {
   barplot.isLeadLag = barplot.isLeadLag ? false : true;
 }
 
-let btn = new ToggleButton('btn-lead-lag', leadLagOnClick, {
-  text: 'Toggle Barplot',
-  parentId: 'column-1',
-});
+populateRankingRadioButtons();
+function populateRankingRadioButtons() {
+  const dataListModel: DataListModel[] = [
+    { id: 0, value: "Arithmetic Mean" },
+    { id: 1, value: "Geometric Mean" },
+    { id: 1, value: "Condorcet Rank" },
+  ]
 
-let datalist = populateRadioButton();
+  return new RadioButton("rank-radiobutton", dataListModel, onClick, {
+    parentId: "column-1",
+  });
 
-/**
- * \brief   Is called when the user clicks on any city radio button.
- *          Rerenders the city bar plot with values form the city.
- * @param   id : Index of the radio button.
- */
-function onClick(id: number) {
-  let city = Data.getSyncData()[id];
-  let name = city.name;
-  header.textContent = name;
-  avgBarplot.applyStrokeByName(name);
-  recenterDashboard();
-  updateAllGraphs(id);
+  /**
+   * \brief   Is called when the user clicks on any rank radio button.
+   *          Rerenders the rank bar plot with ranked values from all cities.
+   * @param   id : Index of the radio button.
+   */
+  function onClick(id: number) {
+    let rankingMethod: Function
+    let meanCountry: DataPoint[]
+    switch (id) {
+      case 0:
+        rankingMethod = Data.getArithmenticMeanForCity;
+        meanCountry = Data.getAverageCountry(rankingMethod);
+        break;
+      case 1:
+        rankingMethod = Data.getPseudoGeometricMeanForCity
+        meanCountry =  Data.getAverageCountry(rankingMethod)
+        break;
+      case 2:
+        meanCountry = Data.get_condorcet_ranking_all_variables();
+      default:
+        break;
+    }
+    avgBarplot.updatePlot(meanCountry);
+    recenterDashboard();
+  }
 }
 
-function populateRadioButton(): RadioButton {
+new ToggleButton("btn-lead-lag", leadLagOnClick, {
+  text: "Toggle Barplot",
+  parentId: "column-1",
+});
+
+populateCityRadioButtons();
+
+function populateCityRadioButtons(): RadioButton {
   let data = Data.getSyncData();
   const dataListModel = data.map((city, id) => {
     return { id: id, value: city.name } as DataListModel;
   });
-  return new RadioButton('cities-radiobutton', dataListModel, onClick, {
-    parentId: 'column-1',
+  return new RadioButton("cities-radiobutton", dataListModel, onClick, {
+    parentId: "column-1",
   });
+
+  /**
+   * \brief   Is called when the user clicks on any city radio button.
+   *          Rerenders the city bar plot with values from the city.
+   * @param   id : Index of the radio button.
+   */
+  function onClick(id: number) {
+    let city = Data.getSyncData()[id];
+    let name = city.name;
+    header.textContent = name;
+    avgBarplot.applyStrokeByName(name);
+    recenterDashboard();
+    updateAllGraphs(id);
+  }
 }
 
 /*********************
@@ -163,7 +197,7 @@ function populateRadioButton(): RadioButton {
 
 //currently set to resize actively, but delays can be set so resize only occurs
 //  at the end the end of screen change if barplot.resize() gets too costly
-$(window).on('resize', function () {
+$(window).on("resize", function () {
   //update d3 barplot
   barplot.resize();
 
