563e8c596a5fccd2bf30a8ecba460da30c657941
jamie-sgro <jamie.sgro@uwaterloo.ca>
2023-05-29 10:56:51 -0400

    updated condorcet with new input data

--- r_script/condorcet_ranking_less_variables.R
+++ r_script/condorcet_ranking_less_variables.R
@@ -2,14 +2,14 @@ library("votesys")
 
 raw <- list2ballot(
   x = list(
-    c("Quebec City","Sherbrooke","Montréal","Calgary","Kitchener--Cambridge--Waterloo","St. John's","Windsor","Saskatoon","Winnipeg","Edmonton","Hamilton","Regina","Halifax","St. Catharines--Niagara","London","Victoria","Vancouver","Toronto"),
-    c("Toronto","Vancouver","Montréal","Regina","Victoria","Calgary","Winnipeg","Kitchener--Cambridge--Waterloo","Quebec City","Edmonton","Saskatoon","Hamilton","St. Catharines--Niagara","London","Sherbrooke","Halifax","Windsor","St. John's"),
-    c("Calgary","Vancouver","Toronto","Victoria","Kitchener--Cambridge--Waterloo","Edmonton","Saskatoon","Winnipeg","London","Regina","Hamilton","Quebec City","Montréal","St. John's","Halifax","Sherbrooke","Windsor","St. Catharines--Niagara"),
-    c("Victoria","Vancouver","Halifax","Sherbrooke","St. John's","Calgary","Winnipeg","Edmonton","Saskatoon","London","St. Catharines--Niagara","Toronto","Montréal","Kitchener--Cambridge--Waterloo","Hamilton","Regina","Quebec City","Windsor"),
-    c("Vancouver","Quebec City","Toronto","Montréal","St. John's","Windsor","Calgary","Victoria","Sherbrooke","St. Catharines--Niagara","London","Regina","Hamilton","Kitchener--Cambridge--Waterloo","Edmonton","Winnipeg","Saskatoon","Halifax")
+    c("Quebec City", "Sherbrooke", "MontrÃ©al", "Calgary", "Kitchener--Cambridge--Waterloo", "St. John's", "Windsor", "Saskatoon", "Winnipeg", "Edmonton", "Hamilton", "Regina", "Halifax", "St. Catharines--Niagara", "London", "Victoria", "Vancouver", "Toronto"),
+    c("Toronto", "Vancouver", "MontrÃ©al", "Regina", "Victoria", "Calgary", "Winnipeg", "Kitchener--Cambridge--Waterloo", "Quebec City", "Edmonton", "Saskatoon", "Hamilton", "St. Catharines--Niagara", "London", "Sherbrooke", "Halifax", "Windsor", "St. John's"),
+    c("Calgary", "Vancouver", "Toronto", "Victoria", "Kitchener--Cambridge--Waterloo", "Edmonton", "Saskatoon", "Winnipeg", "London", "Regina", "Hamilton", "Quebec City", "MontrÃ©al", "St. John's", "Halifax", "Sherbrooke", "Windsor", "St. Catharines--Niagara"),
+    c("Halifax", "Sherbrooke", "Vancouver", "Victoria", "St. John's", "Calgary", "Winnipeg", "Edmonton", "London", "Saskatoon", "St. Catharines--Niagara", "Toronto", "MontrÃ©al", "Kitchener--Cambridge--Waterloo", "Hamilton", "Regina", "Quebec City", "Windsor"),
+    c("Vancouver", "Quebec City", "Toronto", "MontrÃ©al", "St. John's", "Windsor", "Calgary", "Victoria", "Sherbrooke", "St. Catharines--Niagara", "London", "Regina", "Hamilton", "Kitchener--Cambridge--Waterloo", "Edmonton", "Winnipeg", "Saskatoon", "Halifax")
   )
 )
-vote <- create_vote(raw, xtype = 3, candidate = c("Quebec City","Sherbrooke","Montréal","Calgary","Kitchener--Cambridge--Waterloo","St. John's","Windsor","Saskatoon","Winnipeg","Edmonton","Hamilton","Regina","Halifax","St. Catharines--Niagara","London","Victoria","Vancouver","Toronto"))
+vote <- create_vote(raw, xtype = 3, candidate = c("Quebec City","Sherbrooke","Montr?al","Calgary","Kitchener--Cambridge--Waterloo","St. John's","Windsor","Saskatoon","Winnipeg","Edmonton","Hamilton","Regina","Halifax","St. Catharines--Niagara","London","Victoria","Vancouver","Toronto"))
 win1 <- cdc_simple(vote)
 win1
 win1$summary_m
---
+++ r_script/r_ready_condorcet_ranking.py
@@ -0,0 +1,10 @@
+11.1.1
+c("Quebec City", "Sherbrooke", "Montréal", "Calgary", "Kitchener--Cambridge--Waterloo", "St. John's", "Windsor", "Saskatoon", "Winnipeg", "Edmonton", "Hamilton", "Regina", "Halifax", "St. Catharines--Niagara", "London", "Victoria", "Vancouver", "Toronto")
+11.2.1
+c("Toronto", "Vancouver", "Montréal", "Regina", "Victoria", "Calgary", "Winnipeg", "Kitchener--Cambridge--Waterloo", "Quebec City", "Edmonton", "Saskatoon", "Hamilton", "St. Catharines--Niagara", "London", "Sherbrooke", "Halifax", "Windsor", "St. John's")
+11.3.1
+c("Calgary", "Vancouver", "Toronto", "Victoria", "Kitchener--Cambridge--Waterloo", "Edmonton", "Saskatoon", "Winnipeg", "London", "Regina", "Hamilton", "Quebec City", "Montréal", "St. John's", "Halifax", "Sherbrooke", "Windsor", "St. Catharines--Niagara")
+11.6.2
+c("Halifax", "Sherbrooke", "Vancouver", "Victoria", "St. John's", "Calgary", "Winnipeg", "Edmonton", "London", "Saskatoon", "St. Catharines--Niagara", "Toronto", "Montréal", "Kitchener--Cambridge--Waterloo", "Hamilton", "Regina", "Quebec City", "Windsor")
+11.7.2
+c("Vancouver", "Quebec City", "Toronto", "Montréal", "St. John's", "Windsor", "Calgary", "Victoria", "Sherbrooke", "St. Catharines--Niagara", "London", "Regina", "Hamilton", "Kitchener--Cambridge--Waterloo", "Edmonton", "Winnipeg", "Saskatoon", "Halifax")
