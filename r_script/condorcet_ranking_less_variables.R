a661155e441e11fd5d679c24838c949de77abdce
jamie-sgro <jamie.sgro@uwaterloo.ca>
2023-05-30 09:27:04 -0400

    swapped order to see if it makes a difference, it didn't

--- r_script/condorcet_ranking_less_variables.R
+++ r_script/condorcet_ranking_less_variables.R
@@ -5,7 +5,7 @@ raw <- list2ballot(
     c("Quebec City", "Sherbrooke", "Montréal", "Calgary", "Kitchener--Cambridge--Waterloo", "St. John's", "Windsor", "Saskatoon", "Winnipeg", "Edmonton", "Hamilton", "Regina", "Halifax", "St. Catharines--Niagara", "London", "Victoria", "Vancouver", "Toronto"),
     c("Toronto", "Vancouver", "Montréal", "Regina", "Victoria", "Calgary", "Winnipeg", "Kitchener--Cambridge--Waterloo", "Quebec City", "Edmonton", "Saskatoon", "Hamilton", "St. Catharines--Niagara", "London", "Sherbrooke", "Halifax", "Windsor", "St. John's"),
     c("Calgary", "Vancouver", "Toronto", "Victoria", "Kitchener--Cambridge--Waterloo", "Edmonton", "Saskatoon", "Winnipeg", "London", "Regina", "Hamilton", "Quebec City", "Montréal", "St. John's", "Halifax", "Sherbrooke", "Windsor", "St. Catharines--Niagara"),
-    c("Halifax", "Sherbrooke", "Vancouver", "Victoria", "St. John's", "Calgary", "Winnipeg", "Edmonton", "London", "Saskatoon", "St. Catharines--Niagara", "Toronto", "Montréal", "Kitchener--Cambridge--Waterloo", "Hamilton", "Regina", "Quebec City", "Windsor"),
+    c("Victoria","Vancouver","Halifax","Sherbrooke","St. John's","Calgary","Winnipeg","Edmonton","Saskatoon","London","St. Catharines--Niagara","Toronto","Montr�al","Kitchener--Cambridge--Waterloo","Hamilton","Regina","Quebec City","Windsor"),
     c("Vancouver", "Quebec City", "Toronto", "Montréal", "St. John's", "Windsor", "Calgary", "Victoria", "Sherbrooke", "St. Catharines--Niagara", "London", "Regina", "Hamilton", "Kitchener--Cambridge--Waterloo", "Edmonton", "Winnipeg", "Saskatoon", "Halifax")
   )
 )
